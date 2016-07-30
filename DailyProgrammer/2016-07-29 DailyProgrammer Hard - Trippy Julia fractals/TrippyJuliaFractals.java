bbbfe6550fcbaf5025eca34b12fc2de207348aca
Oskar Mendel <brokenprogrammer@gmail.com>
2016-07-31 00:56:38 +0200

    Fixed run speed
    Changed drawing method to be using WritableRaster instead of
    image.setRGB and remove System.out functions to speed up the application
    by a lot.

--- DailyProgrammer/2016-07-29 DailyProgrammer Hard - Trippy Julia fractals/TrippyJuliaFractals.java
+++ DailyProgrammer/2016-07-29 DailyProgrammer Hard - Trippy Julia fractals/TrippyJuliaFractals.java
@@ -1,6 +1,7 @@
 package me.oskarmendel;
 
 import java.awt.image.BufferedImage;
+import java.awt.image.WritableRaster;
 import java.io.File;
 import java.io.IOException;
 
@@ -8,14 +9,14 @@ import javax.imageio.ImageIO;
 
 public class TrippyJuliaFractals {
 	//Our Julia function: f(x) = z^2 (- 0.221 - 0.713) Were () is the constant.
-	static final double REALNUM = -0.4;
-	static final double IMNUM = 0.6;
+	static final double REALNUM = -0.221;
+	static final double IMNUM = -0.713;
 	static final double MINRANGE = -1;
 	static final double MAXRANGE = 1;
-	static final int WIDTH = 500;
-	static final int HEIGHT = 400;
-	static final int THRESHOLD = 2;
-	static final int MAXITERATIONS = 128;
+	static final int WIDTH = 1920; //Image Width
+	static final int HEIGHT = 1080; //Image Height
+	static final int THRESHOLD = 2; //
+	static final int MAXITERATIONS = 128; //Max iterations per pixel determites the detail.
 	
 	static class ComplexNumber {
 		double real;
@@ -40,21 +41,27 @@ public class TrippyJuliaFractals {
 	
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+		long start = System.currentTimeMillis();
 		
+		BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_BYTE_GRAY);
+		WritableRaster wr = (WritableRaster) image.getData();
+		
+		int[] pixels;
 		int x = 0;
 		int y = 0;
 		
 		//Looping through each pixel making complex numbers for all spaces
 		for (y = 0; y < HEIGHT; y++){
 			for (x = 0; x < WIDTH; x++) {
+				//Transforming the coordinates so it lies between the range -1 to 1
 				double a = x*(MAXRANGE-MINRANGE) / WIDTH + MINRANGE;
 				double b = y*(MAXRANGE-MINRANGE) / HEIGHT + MINRANGE;
+				
+				ComplexNumber cn = new ComplexNumber(a, b);
 				
 				//Num of iterations
 				int i;
 				
-				ComplexNumber cn = new ComplexNumber(a, b);
 				//Starting iteration process
 				for (i = 0; i < MAXITERATIONS; i++){
 					cn.applyF();
@@ -62,11 +69,18 @@ public class TrippyJuliaFractals {
 						break;
 					}
 				}
-				System.out.println("X: " + x + " Y: " + y + " a: " + a + " b: " + b + " Rslt: " + cn.getAbs() + " Iterations: " + i);
-				image.setRGB(x, y, i);
+				
+				wr.setSample(x, y, 0, i);
 			}
 		}
 		
+		//Set the content of the raster to the image
+		image.setData(wr);
+		System.out.println("Image is done.");
+		//Write the buffered image to a new file
 		ImageIO.write(image, "png", new File("juliafractal.png"));
+		
+		long end = System.currentTimeMillis();
+		System.out.println((end - start) + " ms");
 	}
 }
