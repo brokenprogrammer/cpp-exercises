5ee7508c73d8025d189890a81cbd40d6f39aa685
Oskar Mendel <brokenprogrammer@gmail.com>
2017-05-05 19:31:24 +0200

    Problem 10 in Haskell 99 Problems

---
+++ Haskell 99 Questions/Problem10.hs
@@ -0,0 +1,15 @@
+-- Problem 10
+-- Run-length encoding of a list. Use the result of problem P09 to implement
+-- the so-called run-length encoding data compression method. Consecutive duplicates
+-- of elements are encoded as lists (N E) where N is the number of duplicates of the
+--element E.
+--
+-- Example:
+--
+-- encode "aaaabccaadeeee"
+-- [(4,'a'),(1,'b'),(2,'c'),(2,'a'),(1,'d'),(4,'e')]
+
+import Data.List
+
+encode :: Eq a => [a] -> [(Int, a)]
+encode xs = map (\x -> (length x,head x)) (group xs)
