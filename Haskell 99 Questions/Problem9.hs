c79f5feb7a5c015ed9e0925d24192f3794f02342
Oskar Mendel <brokenprogrammer@gmail.com>
2017-05-05 19:23:47 +0200

    Problem 9 in Haskell 99 Problems

---
+++ Haskell 99 Questions/Problem9.hs
@@ -0,0 +1,14 @@
+-- Problem 9
+-- Pack consecutive duplicates of list elements into sublists.
+-- If a list contains repeated elements they should be placed in separate sublists.
+--
+-- Example:
+-- pack ['a', 'a', 'a', 'a', 'b', 'c', 'c', 'a', 'a', 'd', 'e', 'e', 'e', 'e']
+-- ["aaaa","b","cc","aa","d","eeee"]
+
+pack :: Eq a => [a] -> [[a]]
+pack [] = []
+pack [x] = [[x]]
+pack (x:xs) = if x `elem` (head (pack xs))
+              then (x:(head (pack xs))):(tail (pack xs))
+              else [x]:(pack xs)
