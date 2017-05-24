c3d7831f344d31b171432f13aa9217e1a4a2bce6
Oskar Mendel <brokenprogrammer@gmail.com>
2017-05-24 09:55:06 +0200

    Updated comments in project euler 4 D

--- ProjectEuler D/ProjectEuler004.d
+++ ProjectEuler D/ProjectEuler004.d
@@ -1,10 +1,11 @@
 /++
-    Largest prime factor
-    Problem 3
+    Largest palindrome product
+    Problem 4
     
-    The prime factors of 13195 are 5, 7, 13 and 29.
-
-    What is the largest prime factor of the number 600851475143 ?
+    A palindromic number reads the same both ways. The largest 
+    palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+    
+    Find the largest palindrome made from the product of two 3-digit numbers.
  +/
 
 import std.stdio;
@@ -28,7 +29,7 @@ void main() {
     }
 
     writeln(largestPalindrome);
- }
+}
 
  bool isPalindrome(string x) {
      
