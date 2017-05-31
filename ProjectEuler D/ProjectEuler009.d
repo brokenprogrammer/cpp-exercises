74e08fac10b8b6d74b84742aeb8d3707419a73c5
Oskar Mendel <brokenprogrammer@gmail.com>
2017-05-31 14:09:17 +0200

    Added comments for project euler 8 & 9 in D

--- ProjectEuler D/ProjectEuler009.d
+++ ProjectEuler D/ProjectEuler009.d
@@ -1,11 +1,18 @@
+/++
+    Special Pythagorean triplet
+    Problem 9
+    
+    A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+        a2 + b2 = c2
+    
+    For example, 32 + 42 = 9 + 16 = 25 = 52.
+    There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+    Find the product abc.
+ +/
 import std.stdio;
 import std.math;
 
 void main() {
-    writeln("Hello World");
-    int m = 1;
-    int n = 2;
-
     for (int a = 1; a < 1000; a++) {
         for (int b = a+1; b < 1000; b++) {
             int c = 1000 - a - b;
