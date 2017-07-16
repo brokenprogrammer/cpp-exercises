63bc910a83f4c647266db781ce90e5792640ce7c
Oskar Mendel <brokenprogrammer@gmail.com>
2017-07-16 12:57:35 +0200

    Exercise 9 Chapter 3 Discrete Math

---
+++ Discrete Mathematics Using a Computer/Chapter 3/Exercise9.hs
@@ -0,0 +1,10 @@
+--  Write a function 
+-- extract :: [Maybe a] -> [a] 
+-- that takes a list of Maybe values and returns the elements they contain. 
+-- For example, 
+-- extract [Just 3, Nothing, Just 7] = [3, 7]. 
+
+extract :: [Maybe a] -> [a]
+extract [] = []
+extract (Just x:xs) = x : extract xs
+extract (_:xs) = extract xs 
