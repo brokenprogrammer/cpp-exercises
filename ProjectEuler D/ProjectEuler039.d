ffc0d7b842e6364ad0a00aaa3139eda922ea4d48
Oskar Mendel <brokenprogrammer@gmail.com>
2018-03-08 05:05:34 +0100

    ProjectEuler 39 D

---
+++ ProjectEuler D/ProjectEuler039.d
@@ -0,0 +1,58 @@
+/+
+    Integer right triangles
+    Problem 39
+
+    If p is the perimeter of a right angle triangle with integral length sides,
+    {a,b,c}, there are exactly three solutions for p = 120.
+
+    {20,48,52}, {24,45,51}, {30,40,50}
+
+    For which value of p ≤ 1000, is the number of solutions maximised?
+
+    This can be solved using arithmetics:
+    We have the following equations:
+        a^2 + b^2 = c^2
+        a + b + c = p
+
+    The second equation can be written as:
+        c = p - a - b
+    Which in turn can be used within the first equation:
+        a^2 + b^2 = (p - a - b)^2
+    Which can be extended to:
+        a^2 + b^2 = p^2 + a^2 + b^2 - 2pa - 2pb + 2ab
+
+    B can be isolated like this:
+        a^2 + b^2 = p^2 + a^2 + b^2 - 2pa - 2pb + 2ab
+                0 = p^2 - 2pa - 2pb + 2ab
+        2pb - 2ab = p^2 - 2pa
+        b(2p - 2a) = p^2 - 2pa
+                b = (p^2 - 2pa)/(2p - 2a)
++/
+
+import std.stdio;
+
+void main () 
+{
+    long answer = 0;
+    long numberSolutions = 0;
+
+    for (long p = 2; p <= 1000; p+=2) 
+    {
+        int solutions = 0;
+        for (long a = 2; a < (p/3); a++)
+        {
+            if (p*(p-2*a) % (2*(p-a)) == 0) 
+            {
+                solutions++;
+            }
+        }
+
+        if (numberSolutions < solutions) 
+        {
+            numberSolutions = solutions;
+            answer = p;
+        }
+    }
+
+    writeln(answer);
+}
